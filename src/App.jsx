78ed1aef0f5aa5eeb26f5990aeaca55813dadfa9
dwitap <dwitap.1223@gmail.com>
2023-04-14 11:40:55 +0700

    krish4alex HTML & CSS

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="upperNav">
+        <div class="item-name">
+          krish<span className="span">4alex</span>
+        </div>
+        <div class="item">Home</div>
+        <div class="item">About</div>
+        <div class="item">Service</div>
+        <div class="item">Skills</div>
+        <div class="item">Blog</div>
+        <div class="item">Contact</div>
+      </div>
+      <div className="container2">
+        <div className="i1">
+          <div className="i1-1">
+            krish<span className="span">4alex</span>
+          </div>
+          <br />
+          <div className="i1-2"> User Interface Designer</div>
+          <br />
+          <div className="i1-3">
+            I’m UI/UX designer in lucknow , and I’m very passionate <br />
+            and dedicated to my work.
+          </div>
+          <br />
+          <button className="hello-button">Hello</button>
+        </div>
+        <div className="i2">pict</div>
+      </div>
+      <div className="scroll">scroll down</div>
+    </div>
+  );
+}
+
+export default App;
